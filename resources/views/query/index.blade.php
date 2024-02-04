1dbb8ccb3ad7e9fd1e43e96753075d4a4f53bfaa
Md Rafiqul Islam <vwaaala@gmail.com>
2024-02-05 04:06:34 +0600

    Update index.blade.php

--- resources/views/query/index.blade.php
+++ resources/views/query/index.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.app')
 @section('content')
-query
+<!-- User DataTable -->
+<div class="card">
+    <div class="card-header">Query</div>
+    <div class="card-body">
+        {{ $dataTable->table() }}
+    </div>
+</div>
 @endsection
+
+@push('scripts')
+{{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+@endpush
