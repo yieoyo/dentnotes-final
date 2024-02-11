36bacb6df0ee886ed3acce2f7ff7ab91ab03cdc8
yieoyo <yieoyo@gmail.com>
2024-02-12 00:41:39 +0600

    Modified js style and home blade to make ui functional

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -22,11 +22,11 @@ class NoteController extends Controller
         if ($validator){
             $note = Note::create(['name' => $request->input('noteName'), 'notes' => $request->input('noteContent'), 'user_id' => auth()->user()->id, 'category_id' => $request->input('categoryId')]);
             if($note){
-                return response()->json(['success'=> 200, 'data'=> $note->id]);
+                return response()->json(['status'=> "success", 'data'=> $note->id]);
             }
-            return response()->json(['error'=> 200, 'message'=>  $request->input('noteName')]);
+            return response()->json(['status'=> "error", 'message'=>  $request->input('noteName')]);
         }
-        return response()->json(['error'=> 200, 'message'=> 'paini']);
+        return response()->json(['status'=> "error", 'message'=> 'paini']);
 
     }
 
@@ -37,9 +37,9 @@ class NoteController extends Controller
     {
         if($request->ajax()){
             $note = Note::findOrfail($request->input('id'));
-            return response()->json(['success'=> 200, 'data'=> $note]);
+            return response()->json(['status'=> "success", 'data'=> $note]);
         }
-        return response()->json(['error'=> 200, 'message'=> 'Error']);
+        return response()->json(['status'=> "error", 'message'=> 'Error']);
     }
 
     /**
@@ -54,18 +54,18 @@ class NoteController extends Controller
             'noteContent' => 'required',
         ]);
         if ($validator->fails()) {
-            return response()->json(['error' => 400, 'message' => $validator->errors()->first()]);
+            return response()->json(['status' => "error", 'message' => $validator->errors()->first()]);
         }
 
-        $note = Note::findOrfail('id', $request->input('id'));
-        if( auth()->user()->isAdmin() || $note->user_id == auth()->user()->id ){
+        $note = Note::findOrfail($request->input('id'));
+        if( $note->user_id == auth()->user()->id ){
             $note = $note->update(['name' => $request->input('noteName'), 'notes' => $request->input('noteContent'), 'category_id' => $request->input('categoryId')]);
             if($note !== false){
-                return response()->json(['success'=> 200, 'message'=> 'Note updated successfully!']);
+                return response()->json(['status'=> "error", 'message'=> 'Note updated successfully!']);
             }
-            return response()->json(['success'=> 200, 'message'=> 'Failed to update!']);
+            return response()->json(['status'=> "error", 'message'=> 'Failed to update!']);
         }
-        return response()->json(['error'=> 200, 'message'=> 'You can not update note!']);
+        return response()->json(['status'=> "error", 'message'=> 'You can not update note!']);
     }
 
     /**
@@ -77,14 +77,14 @@ class NoteController extends Controller
             'id' => 'required|numeric|gt:0',
         ]);
         if ($validator->fails()) {
-            return response()->json(['error' => 400, 'message' => $validator->errors()->first()]);
+            return response()->json(['status' => "error", 'message' => $validator->errors()->first()]);
         }
         $note = Note::findOrfail($request->input('id'));
         if( auth()->user()->isAdmin() || $note->user_id == auth()->user()->id ){
             if($note->delete()){
-                return response()->json(['success'=> 200, 'message'=> 'Note deleted successfully!']);
+                return response()->json(['status'=> "success", 'message'=> 'Note deleted successfully!']);
             }
-            return response()->json(['success'=> 200, 'message'=> 'Failed to delete note!']);
+            return response()->json(['status'=> "error", 'message'=> 'Failed to delete note!']);
         }
         return response()->json(['error'=> 200, 'message'=> 'You can not delete note!']);
     }
